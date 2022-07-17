bcd40f30482a4cacefb2c62ab9e970eb4e0ad7b8
Debarghya Datta <ujan.dattaa@gmail.com>
2022-07-17 15:22:00 +0530

    updates

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController')
 
+//Create
+router.post('/',setGoal)
+
+//Read
+router.get('/',getGoals)
+
+// router.get('/',(req,res)=>{
+//     res.status(200).json({message:'Get Goals'})
+//     // res.send('Get Goals')
+// })
+
+//Update
+router.put('/:id',updateGoal)
+
+//Delete
+router.get('/:id ',deleteGoal)
 
 
 module.exports= router
