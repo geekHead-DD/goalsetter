fa2ed6f7e35d80574b133c37ee748d689f170137
Debarghya Datta <ujan.dattaa@gmail.com>
2022-07-17 15:03:50 +0530

    basic template

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/goals',(req,res)=>{
+    res.status(200).json({message:'Get Goals'})
+    // res.send('Get Goals')
+})
+
+app.listen(port, ()=>console.log(`Server started on port ${port}`))
